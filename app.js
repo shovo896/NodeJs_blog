0afd0efa5bf34fc8d3e6408af638dc1a7d9419af
Shuvo <suvo10.info@gmail.com>
2026-01-12 21:39:06 +0600

    ruur

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const expressLayout= require('express-ejs-layouts');
 const app = express();
 const PORT=5000 || process.env.PORT;
+// templating Engine 
+app.use(expressLayout);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+
 
 app.get('',(req,res) => {
        req.send("Hello world");
