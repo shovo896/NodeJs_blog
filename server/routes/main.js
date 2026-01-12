0833525ab0240310b950266c2be6555644596a99
Shuvo <suvo10.info@gmail.com>
2026-01-12 22:50:21 +0600

    fjjf

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// routes 
+
+
 app.get('',(req,res) => {
        req.send("Hello world");
 });
+
+
+module.exports = router;
