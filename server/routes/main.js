bbe62910f0e73d784fb54d547360850046fcc5b8
Shuvo <suvo10.info@gmail.com>
2026-01-19 14:08:49 +0600

    fbrs

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 // routes
 router.get('', (req, res) => {
+       const locals={
+              title : "NodeJs Blog",
+              description: "Simple blog created with NodeJs and ExpressJs and MongoDB. " 
+       }
+
+
+
+
+
        res.render('index');
 });
 
