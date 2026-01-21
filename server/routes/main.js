c8e5c7317ca53404cc422aa513b401a91acd1fb0
Shuvo <suvo10.info@gmail.com>
2026-01-22 02:02:26 +0600

    j bjhhj

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,42 +9,14 @@ router.get('', (req, res) => {
               title : "NodeJs Blog",
               description: "Simple blog created with NodeJs and ExpressJs and MongoDB. " 
        };
+       try {
+              const data=await.Post.find();
+              res.render('index',{locals,data});
 
+       }  catch(error){
+              console.log(error);
 
-function insertPostData(){
-       postMessage.insertMany([
-              {
-                     title:"First Post",
-                     body:"This is the body of the first post"
-              },
-              {
-                     title:"Second Post",
-                     body:"This is the body of the second post"
-
-              },
-              {
-                     title:"Third Post",
-                     body:"This is the body of the third post"
-
-              } ,
-              {
-                     title:"Fourth Post",
-                     body:"This is the body of the fourth post"
-
-              } ,
-              {
-                     title:"Fifth Post",
-                     body:"This is the body of the fifth post"
-
-              } ,
-              {
-                     title:"Sixth Post",
-                     body:"This is the body of the sixth post"
-
-              }     
-
-       ])};
-       insertPostData();
+       }
 
 
 
@@ -59,7 +31,8 @@ function insertPostData(){
 
 
 
-       res.render('index',{locals});
+
+       
 });
 
 router.get('/about', (req, res) => {
