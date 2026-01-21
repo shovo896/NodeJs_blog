6fd311bb532724337ef9e79dcbff34f514a54dae
Shuvo <suvo10.info@gmail.com>
2026-01-22 02:49:23 +0600

    ygyu

--- server/routes/main.js
+++ server/routes/main.js
@@ -15,6 +15,16 @@ router.get('', async (req, res) => {
        let page=req.query.page || 1;
 
        const data= await Post.aggregate([{$sort:{createAt:-1}}])
+       .skip(perPage * page - perPage)
+       .limit(perPage)
+       .exec();
+
+
+       const count= await Post.countDocuments();
+       const nextPage= parseInt(page)+1;
+       const hasNextPage= count > perPage * page;
+       const previousPage= parseInt(page)-1;
+       const hasPreviousPage= previousPage >=1;
 
 
 
@@ -23,7 +33,7 @@ router.get('', async (req, res) => {
 
 
               
-              res.render('index',{locals,data});
+              res.render('index',{locals,data,current:page,nextPage:hasNextPage,});
 
        }  catch(error){
               console.log(error);
