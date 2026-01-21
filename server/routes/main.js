655a7708c5968560cef61d7d78a7cadd4b89f457
Shuvo <suvo10.info@gmail.com>
2026-01-22 00:09:37 +0600

    vmkkgb

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 // routes
+// home route
 router.get('', (req, res) => {
        const locals={
               title : "NodeJs Blog",
               description: "Simple blog created with NodeJs and ExpressJs and MongoDB. " 
-       }
+       };
+
+
+function insertPostData(){
+       postMessage.insertMany([
+              {
+                     title:"First Post",
+                     body:"This is the body of the first post"
+              },
+              {
+                     title:"Second Post",
+                     body:"This is the body of the second post"
+
+              },
+              {
+                     title:"Third Post",
+                     body:"This is the body of the third post"
+
+              } ,
+              {
+                     title:"Fourth Post",
+                     body:"This is the body of the fourth post"
+
+              } ,
+              {
+                     title:"Fifth Post",
+                     body:"This is the body of the fifth post"
+
+              } ,
+              {
+                     title:"Sixth Post",
+                     body:"This is the body of the sixth post"
+
+              }     
+
+       ])};
+       insertPostData();
+
+
+
+
+
+
+
+
+
 
 
 
