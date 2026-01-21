05a84d1e8390f984ebcbfa72fcf60de0de215482
Shuvo <suvo10.info@gmail.com>
2026-01-21 23:22:00 +0600

    fjdl

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,3 +1,21 @@
 const mongoose= require('mongoose');
 
 const Schema= mongoose.Schema;
+const PostSchema= new Schema({
+       title:{type: String,required:true},
+       body:{type: String,required:true},
+       createAt:{
+              type: Date,
+              default: Date.now
+       
+       },
+       createAt:{
+              type: Date,
+              default: Date.now
+       
+       }
+
+
+
+       
+})
