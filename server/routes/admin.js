e1f27fd1c49da0262f7102856d1636adc1558430
Shuvo <suvo10.info@gmail.com>
2026-01-24 21:14:57 +0600

    vckl

---
+++ server/routes/admin.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Post = require('../models/Post');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
