383afb600c35a45a742b22a434244675da8a0ec3
Shuvo <suvo10.info@gmail.com>
2026-01-25 00:03:17 +0600

    jbkj

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -37,6 +37,28 @@ router.get('/admin/register', async (req, res) => {
 
 
 
+// admin - register POST
+router.post('/admin/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      // Handle the case when the username is already taken
+      return res.status(400).send('Username already exists');
+    }
+
+    // Create a new user
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    res.redirect('/admin');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 
