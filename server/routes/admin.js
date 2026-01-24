d067c9a3816c771d585fb8a5ff46a85b60bd0ead
Shuvo <suvo10.info@gmail.com>
2026-01-24 21:56:51 +0600

    mcml

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Post = require('../models/Post');
-const adminLayout='../views/layouts/admin';
+const adminLayout = 'layouts/admin';
 
 
 // admin - login
