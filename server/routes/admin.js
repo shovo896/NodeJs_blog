2a1019d98cbf4f2799b26f2257b50dc21ed2dba1
Shuvo <suvo10.info@gmail.com>
2026-01-24 21:49:09 +0600

    dfnhdrk

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Post = require('../models/Post');
-const adminLayout='../layouts/admin';
+const adminLayout='../views/layouts/admin';
 
 
 // admin - login
